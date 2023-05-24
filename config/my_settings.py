a6940e561220e049e36fdb9720ffcfabe8195971
heyrin1998 <qq5200@naver.com>
2023-05-24 14:42:04 +0900

    update env

--- config/my_settings.py
+++ config/my_settings.py
@@ -1,13 +1,16 @@
-
+import os
+import dotenv
+env_file = dotenv.find_dotenv()
+dotenv.load_dotenv(env_file)
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'flowery',  # 연동할 mysql db 이름
-        'USER': 'root',  # db 접속 계정명
-        'PASSWORD': 'gP5fls2qkr3!m',  # 해당 계정 비밀번호
+        'USER': os.environ.get("MYSQL_USER"),  # db 접속 계정명
+        'PASSWORD': os.environ.get("MYSQL_PW"),  # 해당 계정 비밀번호
         'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'PORT': os.environ.get("MYSQL_PORT"),
         'OPTIONS': {
             'init_command': 'SET sql_mode="STRICT_TRANS_TABLES"'
         }
